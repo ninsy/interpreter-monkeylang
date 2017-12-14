2d510e42a2e70d626dbc5d9951d8c853605982ec
ninsy <kwojciechowski094@gmail.com>
2017-12-14 01:00:57 +0100

    finished parsing

--- repl/repl.go
+++ repl/repl.go
@@ -1,29 +1,41 @@
 package repl
 
 import (
-  "bufio"
-  "fmt"
-  "io"
-  "monkey/lexer"
-  "monkey/token"
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/parser"
 )
 
 const PROMOT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
-  scanner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in)
 
-  for {
-    fmt.Printf(PROMOT)
-    scanned := scanner.Scan()
-    if !scanned {
-      return
-    }
-    line := scanner.Text()
-    l := lexer.New(line)
+	for {
+		fmt.Printf(PROMOT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
 
-    for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-      fmt.Printf("%+v\n", tok)
-    }
-  }
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
