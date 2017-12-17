b753308de2dd23203fba2c6f996d3b790a3abbd3
ninsy <kwojciechowski094@gmail.com>
2017-12-17 22:33:57 +0100

    evaluation first steps

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,125 @@
+package evaluator
+
+import (
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+		{"-5", -5},
+		{"5 + 5 + 5", 15},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
+		{"3 * ( 3 * 3 ) + 10", 37},
+		{"3 + 3 * 3 / 3", 6},
+		{"-50 + 200 - 100", 50},
+		{"30 * -1 + 15 * 2", 0},
+		{"50 / 2 * 3 - 5", 70},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer, got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value, got=%d, want=%d",
+			obj, expected)
+		return false
+	}
+	return true
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean, got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value, got=%t, want=%t",
+			result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestMinusPrefixExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"-10", -10},
+		{"-5", -5},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testMinusPrefixExpression(t, evaluated, tt.expected)
+	}
+}
+
+func testMinusPrefixExpression(t *testing.T, obj object.Object, expected interface{}) bool {
+	switch which := expected.(type) {
+	case int64:
+		return testIntegerObject(t, obj, int64(which))
+	default:
+		return false
+	}
+}
